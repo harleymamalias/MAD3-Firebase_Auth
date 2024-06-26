b2192399ac72c539c5eecc6ed9f838dd137e3762
Harley Mamalias <harleymamalias@gmail.com>
2024-06-26 17:44:39 +0800

    Initial project commit with established firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:state_change_demo/firebase_options.dart';
 import 'package:state_change_demo/src/controllers/auth_controller.dart';
 import 'package:state_change_demo/src/routing/router.dart';
 
-void main() async{
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   AuthController.initialize();
   GlobalRouter.initialize();
   await AuthController.I.loadSession();
